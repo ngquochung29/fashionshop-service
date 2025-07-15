7cd80ab6af20fc601dfad196661766afc3066f2c
Nguyen Quoc Hung <email@domain.com>
2025-07-15 15:18:33 +0700

    - HungNQ innit project

---
+++ src/main/java/shop/server/service/ProductService.java
@@ -0,0 +1,9 @@
+package shop.server.service;
+
+/**
+ * @author : duynd14
+ * @mailto : hungnqdatn04@gmail.com
+ * @created : 15/7/2025, 
+ **/
+public interface ProductService {
+}
---
+++ src/main/java/shop/server/service/impl/ProductServiceImpl.java
@@ -0,0 +1,9 @@
+package shop.server.service.impl;
+
+/**
+ * @author : duynd14
+ * @mailto : hungnqdatn04@gmail.com
+ * @created : 15/7/2025, 
+ **/
+public class ProductServiceImpl {
+}
