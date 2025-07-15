777b3c2c526ceb1ed57bdd638ffb92a4c99bccd0
Nguyen Quoc Hung <hungnqdatn04@gmail.com>
2025-07-15 16:20:23 +0700

    - HungNQ innit project

--- src/main/java/shop/server/controller/ProductController.java
+++ src/main/java/shop/server/controller/ProductController.java
@@ -2,10 +2,9 @@ package shop.server.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.web.bind.annotation.*;
+import shop.server.model.dto.ApiBaseResp;
 import shop.server.model.dto.ProductDto;
 import shop.server.service.ProductService;
 
@@ -16,14 +15,35 @@ import shop.server.service.ProductService;
  **/
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/v1/internal")
 @RequiredArgsConstructor
 @Slf4j
 public class ProductController {
     private final ProductService productService;
 
-    @GetMapping("/{code}")
-    public ProductDto getProduct(@PathVariable String code) {
+    // lay DS sp
+    @GetMapping("/list")
+    public ApiBaseResp list(@RequestParam SpringDataWebProperties.Pageable pageable, @RequestParam String search) {
+        return new ApiBaseResp();
+    }
 
+    //Lay SP theo ma Code
+    @GetMapping("/{code}")
+    public ApiBaseResp getProduct(@PathVariable String code) {
+        return new ApiBaseResp(productService.getProductDtoByCode(code));
+    }
+
+    //Them, Sua SP
+    @PostMapping("/")
+    public ApiBaseResp saveProduct(@RequestBody ProductDto productDto) {
+        productService.saveProduct(productDto);
+        return new ApiBaseResp();
+    }
+
+    // xoa SP
+    @DeleteMapping("/{code}")
+    public ApiBaseResp deleteProduct(@PathVariable String code){
+        productService.deleteProduct(code);
+        return new ApiBaseResp();
     }
 }
---
+++ src/main/java/shop/server/model/dto/ApiBaseResp.java
@@ -0,0 +1,29 @@
+package shop.server.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.sql.Timestamp;
+
+/**
+ * @author : Nguyen Quoc Hung
+ * @mailto : hungnqdatn04@gmail.com
+ * @created : 15/7/2025,
+ **/
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiBaseResp {
+    private HttpStatus status = HttpStatus.OK;
+    private Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+    private Object data;
+
+    public ApiBaseResp(Object data) {
+        this.data = data;
+    }
+}
