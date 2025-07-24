414405ce0d49fa0ca6c353b18f145ab62ad3085d
Nguyen Quoc Hung <hungnqdatn04@gmail.com>
2025-07-24 09:32:27 +0700

    - update config

---
+++ src/main/java/shop/server/webconfig/WebConfig.java
@@ -0,0 +1,22 @@
+package shop.server.webconfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author : Nguyen Quoc Hung
+ * @mailto : hungnqdatn04@gmail.com
+ * @created : 24/7/2025,
+ **/
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // cho phép tất cả path
+                .allowedOrigins("*") // hoặc "*" nếu dev
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
